fd48c4185ef35d1729612ef239c1ab019d6e1a7a
Seb Stuck <sebas7236@icloud.com>
2023-03-15 12:42:11 -0600

    Add project files.

---
+++ 1_27 C++ Check for Understanding/Source.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	cout << "IF STATEMENTS -------------------------------" << endl << endl;
+	cout << "how many cookies ----------------------------" << endl << endl;
+	int cookienumber;
+	cout << "how many cookies do you have" << endl;
+	cin >> cookienumber;
+		if (cookienumber < 5)
+			cout << "here, have a cookie" << endl;
+		else if (cookienumber < 10)
+			cout << "thats a perfect amount of cookies" << endl;
+		else
+			cout << "you have too many cookies. I want one" << endl;
+	cout << endl;
+	cout << "favorite character --------------------------" << endl << endl;
+	cout << "who do you like best: (b)art simpson, (s)cooby doo, (r)ick, (d)affy duck, or (p)ac man" << endl;
+	char characterchoice;
+	cin >> characterchoice;
+	if (characterchoice == 'b')
+		cout << "eat my shorts" << endl;
+	else if (characterchoice == 's')
+		cout << "scooby dooby doo" << endl;
+	else if (characterchoice == 'r')
+		cout << "wubba lubba dub dub" << endl;
+	else if (characterchoice == 'd')
+		cout << "sufferin' succotash!" << endl;
+	else if (characterchoice == 'p')
+		cout << "waca waca" << endl;
+	else
+		cout << "not an option" << endl;
+	cout << endl;
+	cout << "who do you like best: (b)art simpson, (s)cooby doo, (r)ick, (d)affy duck, or (p)ac man" << endl;
+	char characterchoice2;
+	cin >> characterchoice2;
+	switch (characterchoice2) {
+	case 'b':
+		cout << "eat my shorts" << endl;
+		break;
+	case 's':
+		cout << "scooby dooby doo" << endl;
+		break;
+	case 'r':
+		cout << "wubba lubba dub dub" << endl;
+		break;
+	case 'd':
+		cout << "sufferin' succotash!" << endl;
+		break;
+	case 'p':
+		cout << "waca waca" << endl;
+		break;
+	default:
+		cout << "not an option" << endl;
+		break;
+	}
+	
+	cout << endl;
+	cout << "candy ---------------------------------------" << endl << endl;
+	cout << "do you want ice cream (1) or candy (2)" << endl;
+	int INPUT1;
+	cin >> INPUT1;
+	char INPUT2;
+	if (INPUT1 == 1) {
+		cout << "do you want chocolate (a) or fruit (b)" << endl;
+		cin >> INPUT2;
+		if (INPUT2 == 'a')
+			cout << "hot fudge sundae" << endl;
+		if (INPUT2 == 'b')
+			cout << "strawberry blizzard" << endl;
+		else
+			cout << "not an option" << endl;
+	}
+	else if (INPUT1 == 2) {
+		cout << "do you want chocolate (a) or fruit (b)" << endl;
+		cin >> INPUT2;
+		if (INPUT2 == 'a')
+			cout << "hershey bar" << endl;
+		if (INPUT2 == 'b')
+			cout << "starbursts" << endl;
+	}
+	else
+		cout << "not an option" << endl;
+
+}
+
+//⠀⠀⠘⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀
+//⠀⠀⠀⠑⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⠁⠀⠀⠀
+//⠀⠀⠀⠀⠈⠢⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠴⠊⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢀⣀⣀⣀⣀⣀⡀⠤⠄⠒⠈⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠘⣀⠄⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀
+//⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠋⠉⠈⠉⠉⠉⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿
+//⣿⣿⣿⣿⡏⣀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿
+//⣿⣿⣿⢏⣴⣿⣷⠀⠀⠀⠀⠀⢾⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿
+//⣿⣿⣟⣾⣿⡟⠁⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣷⢢⠀⠀⠀⠀⠀⠀⠀⢸⣿
+//⣿⣿⣿⣿⣟⠀⡴⠄⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⣿
+//⣿⣿⣿⠟⠻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶⢴⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⣿
+//⣿⣁⡀⠀⠀⢰⢠⣦⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⡄⠀⣴⣶⣿⡄⣿
+//⣿⡋⠀⠀⠀⠎⢸⣿⡆⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠗⢘⣿⣟⠛⠿⣼
+//⣿⣿⠋⢀⡌⢰⣿⡿⢿⡀⠀⠀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣧⢀⣼
+//⣿⣿⣷⢻⠄⠘⠛⠋⠛⠃⠀⠀⠀⠀⠀⢿⣧⠈⠉⠙⠛⠋⠀⠀⠀⣿⣿⣿⣿⣿
+//⣿⣿⣧⠀⠈⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠀⠀⠀⠀⢀⢃⠀⠀⢸⣿⣿⣿⣿
+//⣿⣿⡿⠀⠴⢗⣠⣤⣴⡶⠶⠖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡸⠀⣿⣿⣿⣿
+//⣿⣿⣿⡀⢠⣾⣿⠏⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠉⠀⣿⣿⣿⣿
+//⣿⣿⣿⣧⠈⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿
+//⣿⣿⣿⣿⡄⠈⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⣦⣄⣀⣀⣀⣀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠙⣿⣿⡟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠁⠀⠀⠹⣿⠃⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣿⣿⠿⠛⠉⠉⠁⠀⢻⣿⡇⠀⠀⠀⠀⠀⠀⢀⠈⣿⣿⡿⠉⠛⠛⠛⠉⠉
+//⣿⡿⠋⠁⠀⠀⢀⣀⣠⡴⣸⣿⣇⡄⠀⠀⠀⠀⢀⡿⠄⠙⠛⠀⣀⣠⣤⣤⠄
